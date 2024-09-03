685d70e22fe20446ba318087c8133455693093c8
Samuel <samuel.gfries@gmail.com>
2024-09-03 12:30:35 -0300

    AnalizaComplexidade

---
+++ Exercicios/ex_3/src/AnalizaComplexidade.java
@@ -0,0 +1,28 @@
+package ex_3.src;
+
+public class AnalizaComplexidade {
+    public static void main(String[] args) {
+        int v = 15;
+        int result = countSequences(v);
+        System.out.println("Número de sequências que somam exatamente " + v + ": " + result);
+    }
+
+    public static int countSequences(int v) {
+        int count = 0;
+
+        for (int start = 1; start <= v; start++) {
+            int sum = 0;
+            for (int i = start; i <= v; i++) {
+                sum += i;
+                if (sum == v) {
+                    count++;
+                    break;
+                } else if (sum > v) {
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+}
